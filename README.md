21b311959df4b0eb68013a91acf2289e0ee3b264
Thomas Rogers <thomasrogerswebdev@gmail.com>
2019-07-28 09:58:57 +0100

    Set up node.js API

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ContactKeeper
- This is an app to manage your contacts. This project is my intro to node.js with react on the front end
+
+This is an app to manage your contacts. This project is my intro to node.js with react on the front end
+
+Just setting up the API with node.JS.
+MongoDB Atlas for database.
---
+++ server.js
@@ -0,0 +1,12 @@
+//entry point to the backend
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'welcome to the contactKeeper API' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
